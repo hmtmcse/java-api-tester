d66bd5b852a7303d7e35ffe5d7172f4505225087
Touhid Mia <hmtm.cse@gmail.com>
2018-11-11 23:43:55 +0600

    Assertion Added

---
+++ src/main/java/com/hmtmcse/apitester/ATAndOrCondition.java
@@ -0,0 +1,23 @@
+package com.hmtmcse.apitester;
+
+public class ATAndOrCondition {
+
+    public Boolean orCondition = false;
+    public Boolean andCondition = false;
+
+    public Boolean getOrCondition() {
+        return orCondition;
+    }
+
+    public void setOrCondition(Boolean orCondition) {
+        this.orCondition = orCondition;
+    }
+
+    public Boolean getAndCondition() {
+        return andCondition;
+    }
+
+    public void setAndCondition(Boolean andCondition) {
+        this.andCondition = andCondition;
+    }
+}
